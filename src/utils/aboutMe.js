960c211eae8809a27b1e6f3e8a2fed1e77e5d7af
Oscar2145 <74516013+Oscar2145@users.noreply.github.com>
2023-08-21 17:35:12 -0500

    fixing about me page

---
+++ src/utils/aboutMe.js
@@ -0,0 +1,17 @@
+export const AboutMeData =[{
+    id:1,
+    bio: "My name is Oscar Leal, and I recently graduated from the University of Texas with dual Bachelor of Science degrees in Arts & Entertainment Technologies and Corporate Communications. Additionally, I successfully completed a full-stack coding bootcamp, where I gained expertise in utilizing various web technologies to build comprehensive applications. The bootcamp was an intensive learning experience that taught me how to effectively navigate both front-end and back-end technologies.",
+    p2: "Equipped with this knowledge, I am proficient in creating full-stack applications. Furthermore, the bootcamp provided valuable opportunities to collaborate with peers, fostering strong teamwork and collaboration skills essential for developing successful projects.",
+    p3: "I am passionate about leveraging my education and coding skills to contribute to innovative and impactful projects in the field of technology. I am excited about the opportunities that lie ahead and look forward to bringing my expertise to the table in a collaborative and dynamic environment."
+},
+];
+
+
+
+
+
+
+
+
+
+
