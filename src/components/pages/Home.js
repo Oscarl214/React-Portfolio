a34053800a0c16beae2bde106a701383d562ec91
Oscar2145 <74516013+Oscar2145@users.noreply.github.com>
2023-08-29 00:04:39 -0500

    created a video data file

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,49 @@
 import React, { useEffect, useRef} from "react";
-import {
-  AiOutlineFileText,
-  AiFillLinkedin,
-  AiFillGithub,
-} from "react-icons/ai";
 import { gsap } from "gsap";
 import "../../styles/app.css";
+import Footer from "../Footer";
 
+import { videoData } from "../../utils/homeutils";
 export default function Home() {
 
 
 
+  useEffect(()=>{
+    const hero =document.querySelector(".hero")
+
+
+    gsap.to(hero, {
+      yPercent: -20,
+      ease: "power1.out",
+      scrollTrigger:{
+        trigger:{
+          trigger: ".home-page",
+          start: "top top",
+          end: "bottom top",
+          scrub:true,
+        }
+      }
+    })
+  }, [])
   
+  useEffect(()=>{
+    const hero =document.querySelector(".hero2")
+
+
+    gsap.to(hero, {
+      yPercent: -40,
+      ease: "power1.out",
+      scrollTrigger:{
+        trigger:{
+          trigger: ".home-page",
+          start: "top top",
+          end: "bottom top",
+          scrub:true,
+        }
+      }
+    })
+  }, [])
+
   useEffect(() => {
     gsap.fromTo(
       ".links",
@@ -20,8 +52,10 @@ export default function Home() {
     );
   });
 
+
   return (
     <div className="text-center p-10 py-10 ">
+      <section className="other-content">
       <h2 className=" animate-pulse text-5xl py-2 text-lime-500 font-medium dark:text-teal-400 md:text-6xl">
         Hi, I'm Oscar 
       </h2>
@@ -29,43 +63,19 @@ export default function Home() {
       <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
         Software Developer.
       </h3>
+      <section className="hero">
       <p className="App-text md py-5 leading-9 max-w-xl mx-auto md:text-x4 font-bold">
         Dedicated to creating web applications that solve real world problems.
         <br></br>
         Lets Connect!
       </p>
-      <div className="text-5xl flex justify-center gap-16 py-3 text-white-600 dark:text-white-400 links">
-        <a
-          href="www.linkedin.com/in/oscar-l-8371a512a"
-          target="_blank"
-          alt="LinkedIn"
-          title="LinkediN"
-        >
-          <AiFillLinkedin />
-        </a>
-        <a
-          href="https://github.com/Oscarl214"
-          title="Github"
-          target="_blank"
-          alt="Github"
-          className="decoration-none"
-        >
-          <AiFillGithub />
-        </a>
-        <a
-          href="https://drive.google.com/file/d/17-0lqDk_KMBrRq-iD69BnZaCOB8diu4X/view?usp=drive_link"
-          target="_blank"
-          alt="drive"
-          title="Resume"
-        >
-          {" "}
-          <AiOutlineFileText />
-        </a>
-      </div>
+        </section>
+      </section>
+
         <div className="nav-container">
-    
+      <section className="hero2">
       <div className=" mx-auto bg-gradient-to-b from-blue-300-500 rounded-full w-60 h-60 relative overflow-hidden mt-20 md:h-100 md:w-100">
-       
+
 
         <img
           src="/Me.jpeg"
@@ -73,6 +83,12 @@ export default function Home() {
           className="w-full h-full object-cover"
           />
           </div>
+          </section>
+          
+      <div className="py-5">
+
+      <Footer />
+      </div>
           </div>
       </div>
    
---
+++ src/utils/homeutils.js
@@ -0,0 +1,7 @@
+export const videoData=[
+    {
+        id:1,
+        videoname: "Open Neon Sign",
+        url: "https://portfolioprojectsol.s3.us-east-2.amazonaws.com/bgneonsign.gif",
+    },
+];
